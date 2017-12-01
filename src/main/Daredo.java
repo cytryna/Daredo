74ba61abc7376fccb8560e02d874be9186546a52
Aleksander Święcicki <aswiecicki@efigence.com>
2017-12-01 08:18:32 +0100

    proba github comitu

--- src/main/Daredo.java
+++ src/main/Daredo.java
@@ -5,6 +5,7 @@ public class Daredo {
 
     public static void main(String[] args) {
 
+        System.out.println("Scooby-Doo, wher are you ?");
         System.err.println("HelloWorld");
     }
 }
